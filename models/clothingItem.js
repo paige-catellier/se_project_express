61405ee4a36d5833747c2a7b825bf2a79fb1948d
paigecatellier <paigemarie454@yahoo.com>
2026-02-03 17:04:17 -0500

    update 2 edits

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const clothingItemSchema = new monngoose.Schema({
+const clothingItemSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
