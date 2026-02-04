389514dba570930e20b9d6757f3519ec4fbee5b3
paigecatellier <paigemarie454@yahoo.com>
2026-02-04 15:49:04 -0500

    edit 3

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
+
 const app = express();
 const { PORT = 3001 } = process.env;
-const indexRouter = require("./routes/index");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
