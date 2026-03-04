d325ad3c91423532860737a14402f32e1bc5bd36
paigecatellier <paigemarie454@yahoo.com>
2026-03-04 18:03:05 -0500

    updates

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
+app.use(helmet());
+
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+app.use(limiter);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
